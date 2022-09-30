8a6acc83ad0fff20ce6ba2abe4dcb6d5c0b1910a
Decktop <2371270263@qq.com>
2022-09-30 12:03:30 +0800

    2022-09-30 1.添加简单的按键布局

--- main.c
+++ main.c
@@ -34,6 +34,201 @@ static int tick_thread(void *data);
  *  STATIC VARIABLES
  **********************/
 
+
+lv_event_cb_t OKKeyProcess(lv_event_t *Event)
+{
+    lv_event_code_t Code = lv_event_get_code(Event);
+
+    if( Code == LV_EVENT_CLICKED)
+    {
+        printf("OK !! \r\n");
+    }
+}
+
+
+lv_event_cb_t DownKeyProcess(lv_event_t *Event)
+{
+    lv_event_code_t Code = lv_event_get_code(Event);
+
+    if( Code == LV_EVENT_CLICKED)
+    {
+        printf("Down !! \r\n");
+    }
+}
+
+lv_event_cb_t UpKeyProcess(lv_event_t *Event)
+{
+    lv_event_code_t Code = lv_event_get_code(Event);
+
+    if( Code == LV_EVENT_CLICKED)
+    {
+        printf("Up !! \r\n");
+    }
+}
+
+lv_event_cb_t BackKeyProcess(lv_event_t *Event)
+{
+    lv_event_code_t Code = lv_event_get_code(Event);
+
+    if( Code == LV_EVENT_CLICKED)
+    {
+        printf("Back !! \r\n");
+    }
+}
+
+
+void Btn()
+{
+    lv_obj_t *Btn = lv_btn_create(lv_scr_act());
+    lv_obj_align(Btn,LV_ALIGN_CENTER,0,0);
+    lv_obj_set_size(Btn,100,100);
+    lv_obj_set_style_bg_color(Btn,lv_palette_main( LV_PALETTE_BLUE),0);
+    lv_obj_set_style_bg_grad_color(Btn, lv_palette_main(LV_PALETTE_RED), 0);
+    lv_obj_set_style_bg_grad_dir(Btn, LV_GRAD_DIR_HOR, 0);
+    lv_obj_set_style_border_width(Btn,3,LV_PART_MAIN);
+    lv_obj_set_style_shadow_color(Btn,lv_color_black(),0);
+    lv_obj_set_style_shadow_ofs_x(Btn,3,0);
+    lv_obj_add_flag(Btn,LV_OBJ_FLAG_CHECKABLE);
+    // lv_obj_add_event_cb(Btn,Btn_Handle,LV_EVENT_ALL,NULL);
+
+    lv_obj_t *label_A = lv_label_create(Btn);
+    lv_label_set_text(label_A,"1 2 3 4 5 6 7 8 9 ");
+    lv_obj_set_align(label_A,LV_ALIGN_TOP_MID);
+    lv_label_set_long_mode(label_A, LV_LABEL_LONG_SCROLL );
+    lv_obj_set_size(label_A,40,50);
+
+
+    lv_obj_t *label_B = lv_label_create(Btn);
+    lv_label_set_text(label_B,"1 2 3 4 5 6 7 8 9 ");
+    lv_obj_set_align(label_B,LV_ALIGN_BOTTOM_MID);
+    lv_label_set_long_mode(label_B, LV_LABEL_LONG_SCROLL );
+    lv_obj_set_size(label_B,40,50);
+
+}
+
+
+void  Btn_OK()
+{
+    lv_obj_t *Btn_OK = lv_btn_create(lv_scr_act());
+    lv_obj_t *Lable = lv_label_create(Btn_OK);
+
+    lv_obj_set_size(Btn_OK,50,20);
+    lv_obj_set_pos(Btn_OK,10,180);
+    lv_obj_set_style_border_side(Btn_OK,LV_BORDER_SIDE_FULL,LV_PART_MAIN);
+    lv_obj_set_style_border_width(Btn_OK,2,LV_PART_MAIN);
+
+    lv_obj_set_style_shadow_color(Btn_OK,lv_color_white(),LV_PART_MAIN );
+    lv_obj_set_style_shadow_ofs_x(Btn_OK,2,LV_PART_MAIN );
+    lv_obj_set_style_shadow_ofs_y(Btn_OK,2,LV_PART_MAIN );
+    lv_obj_set_style_shadow_color(Btn_OK,lv_color_black(),LV_PART_MAIN | LV_STATE_PRESSED);
+
+    lv_label_set_text(Lable,"OK");
+    lv_obj_set_align(Lable,LV_ALIGN_CENTER);
+
+    lv_obj_add_event_cb(Btn_OK, OKKeyProcess ,LV_EVENT_CLICKED,NULL);
+
+}
+
+void  Btn_BACK()
+{
+    lv_obj_t *Btn_BACK = lv_btn_create(lv_scr_act());
+
+    lv_obj_t *Lable = lv_label_create(Btn_BACK);
+
+    lv_obj_set_size(Btn_BACK,50,20);
+    lv_obj_set_pos(Btn_BACK,180,180);
+    lv_obj_set_style_border_side(Btn_BACK,LV_BORDER_SIDE_FULL,LV_PART_MAIN);
+    lv_obj_set_style_border_width(Btn_BACK,2,LV_PART_MAIN);
+
+    lv_obj_set_style_shadow_color(Btn_BACK,lv_color_white(),LV_PART_MAIN );
+    lv_obj_set_style_shadow_ofs_x(Btn_BACK,2,LV_PART_MAIN );
+    lv_obj_set_style_shadow_ofs_y(Btn_BACK,2,LV_PART_MAIN );
+    lv_obj_set_style_shadow_color(Btn_BACK,lv_color_black(),LV_PART_MAIN | LV_STATE_PRESSED);
+
+    lv_label_set_text(Lable,"Back");
+    lv_obj_set_align(Lable,LV_ALIGN_CENTER);
+
+    lv_obj_add_event_cb(Btn_BACK, BackKeyProcess ,LV_EVENT_CLICKED,NULL);
+
+
+}
+
+void  Btn_UP()
+{
+    lv_obj_t *Btn_UP = lv_btn_create(lv_scr_act());
+    lv_obj_t *Lable = lv_label_create(Btn_UP);
+
+    lv_obj_set_size(Btn_UP,50,20);
+    lv_obj_set_pos(Btn_UP,10,210);
+    lv_obj_set_style_border_side(Btn_UP,LV_BORDER_SIDE_FULL,LV_PART_MAIN);
+    lv_obj_set_style_border_width(Btn_UP,2,LV_PART_MAIN);
+
+    lv_obj_set_style_shadow_color(Btn_UP,lv_color_white(),LV_PART_MAIN );
+    lv_obj_set_style_shadow_ofs_x(Btn_UP,2,LV_PART_MAIN );
+    lv_obj_set_style_shadow_ofs_y(Btn_UP,2,LV_PART_MAIN );
+    lv_obj_set_style_shadow_color(Btn_UP,lv_color_black(),LV_PART_MAIN | LV_STATE_PRESSED);
+
+    lv_label_set_text(Lable,"Up");
+    lv_obj_set_align(Lable,LV_ALIGN_CENTER);
+
+    lv_obj_add_event_cb(Btn_UP, UpKeyProcess ,LV_EVENT_CLICKED,NULL);
+}
+
+
+
+
+
+
+
+void  Btn_DOWN()
+{
+    lv_obj_t *Btn_DOWN = lv_btn_create(lv_scr_act());
+    lv_obj_t *Lable = lv_label_create(Btn_DOWN);
+
+    lv_obj_set_size(Btn_DOWN,50,20);
+    lv_obj_set_pos(Btn_DOWN,180,210);
+
+    lv_obj_set_style_border_side(Btn_DOWN,LV_BORDER_SIDE_FULL,LV_PART_MAIN);
+    lv_obj_set_style_border_width(Btn_DOWN,2,LV_PART_MAIN);
+
+    lv_obj_set_style_shadow_color(Btn_DOWN,lv_color_white(),LV_PART_MAIN );
+    lv_obj_set_style_shadow_ofs_x(Btn_DOWN,2,LV_PART_MAIN );
+    lv_obj_set_style_shadow_ofs_y(Btn_DOWN,2,LV_PART_MAIN );
+    lv_obj_set_style_shadow_color(Btn_DOWN,lv_color_black(),LV_PART_MAIN | LV_STATE_PRESSED);
+
+    lv_label_set_text(Lable,"Down");
+    lv_obj_set_align(Lable,LV_ALIGN_CENTER);
+
+    lv_obj_add_event_cb(Btn_DOWN, DownKeyProcess ,LV_EVENT_CLICKED,NULL);
+}
+
+
+void BackGroung()
+{
+    lv_obj_t *Bg = lv_obj_create(lv_scr_act());
+     lv_obj_set_size(Bg,240,240);
+    lv_obj_set_style_bg_color(Bg,lv_color_black(),LV_PART_MAIN);
+    lv_obj_set_style_bg_grad_color(Bg, lv_palette_main(LV_PALETTE_GREY), 0);
+    lv_obj_set_style_bg_grad_dir(Bg, LV_GRAD_DIR_VER, 0);
+}
+
+
+
+
+void Btn_4()
+{
+    Btn_OK();
+    Btn_BACK();
+    Btn_UP();
+    Btn_DOWN();
+    Btn();
+}
+
+
+
+
+
+
 /**********************
  *      MACROS
  **********************/
@@ -54,13 +249,17 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR szCmdLi
 
     /*Run the demo*/
     // lv_demo_widgets();
-    lv_demo_benchmark();
-    
+    //lv_demo_benchmark();
+    //Btn();
+    // Bar();
+    // Bar_Static();
+    BackGroung();
+    Btn_4();
     while(!lv_win32_quit_signal) {
         /* Periodically call the lv_task handler.
          * It could be done in a timer interrupt or an OS task too.*/
         lv_task_handler();
-        usleep(10000);       /*Just to let the system breath*/
+        usleep(10);       /*Just to let the system breath*/
     }
     return 0;
 }
