ff966c75768f4526c256529931df5f68d509a928
Decktop <2371270263@qq.com>
2022-11-15 17:10:46 +0800

    1.添加 Lable 说明

--- LVGL_UI.c
+++ LVGL_UI.c
@@ -11,6 +11,11 @@
 #endif
 
 
+
+#define UI_SIZE    (100)
+
+
+
 lv_obj_t *Arc_Out;
 lv_obj_t *ALL_Bg;
 lv_obj_t *Arc_In;
@@ -19,7 +24,7 @@ lv_group_t *Group;
 lv_anim_t Anima;
 lv_obj_t *Btn;
 lv_obj_t *Btn_Start;
-lv_obj_t *text;
+lv_obj_t *text,*Atext;
 BarState_e BarState = Charing;
 
 
@@ -53,7 +58,7 @@ void Btn_CB(lv_event_t *e)
         if( BarState == Full)
         {
             lv_anim_set_values(&Anima,0,100);
-            lv_anim_set_time(&Anima, 2000);
+            lv_anim_set_time(&Anima, 3000);
             lv_anim_set_exec_cb(&Anima, BarAnimation_CB);
             lv_anim_set_repeat_count(&Anima, 1);
             lv_anim_start(&Anima);
@@ -127,7 +132,6 @@ void Arc_In_CB(lv_event_t *e)
                 lv_anim_start(&Anima);
             break;
         case InRelease:
-
             break;
         default:
             break;
@@ -142,10 +146,15 @@ void StateBar()
     ALL_Bg = lv_obj_create(lv_scr_act());
     lv_obj_set_size(ALL_Bg,240,240);
     lv_obj_set_style_bg_color(ALL_Bg,lv_color_black(),LV_PART_MAIN);
+    lv_obj_set_style_radius(ALL_Bg,0,LV_PART_MAIN);
+    lv_obj_set_style_border_side(ALL_Bg,LV_BORDER_SIDE_FULL,LV_PART_MAIN);
+    lv_obj_set_style_border_color(ALL_Bg,lv_color_white(),LV_PART_MAIN);
+    lv_obj_set_style_border_width(ALL_Bg,0,LV_PART_MAIN);
+    lv_obj_set_scrollbar_mode(ALL_Bg,LV_SCROLLBAR_MODE_OFF);
+
 
     Arc_Out =lv_arc_create(ALL_Bg); 
-
-    lv_obj_set_size(Arc_Out,100,100);
+    lv_obj_set_size(Arc_Out,UI_SIZE,UI_SIZE);
     lv_obj_set_style_arc_color(Arc_Out,lv_palette_main(LV_PALETTE_GREY),LV_PART_MAIN);
     lv_obj_set_style_arc_opa(Arc_Out,LV_OPA_50,LV_PART_MAIN);
     lv_obj_remove_style(Arc_Out, NULL, LV_PART_KNOB);//É¾³ý°ÑÊÖ
@@ -154,10 +163,10 @@ void StateBar()
     lv_obj_set_style_arc_width(Arc_Out,10,LV_PART_MAIN);
     lv_arc_set_value(Arc_Out,0);
     lv_obj_align(Arc_Out,LV_ALIGN_BOTTOM_LEFT,-5,10);
-
+    lv_obj_add_event_cb(Arc_Out,Arc_Out_CB,LV_EVENT_DRAW_MAIN_END,NULL);
 
     Arc_In =lv_arc_create(ALL_Bg); 
-    lv_obj_set_size(Arc_In,75,75);
+    lv_obj_set_size(Arc_In,UI_SIZE-25,UI_SIZE-25);
     lv_obj_align_to(Arc_In,Arc_Out,LV_ALIGN_CENTER,0,0);
     lv_obj_set_style_arc_color(Arc_In,lv_palette_main(LV_PALETTE_GREY),LV_PART_MAIN);
     lv_obj_set_style_arc_opa(Arc_In,LV_OPA_50,LV_PART_MAIN);
@@ -172,7 +181,7 @@ void StateBar()
 
 
     Center = lv_obj_create(ALL_Bg); 
-    lv_obj_set_size(Center,55,55);
+    lv_obj_set_size(Center,UI_SIZE-45,UI_SIZE-45);
     lv_obj_align_to(Center,Arc_In,LV_ALIGN_CENTER,0,0);
     lv_obj_set_style_radius(Center,LV_RADIUS_CIRCLE,0);
     lv_obj_set_style_opa(Center,LV_OPA_50,LV_PART_MAIN);
@@ -184,28 +193,34 @@ void StateBar()
     lv_obj_set_style_text_color(text,lv_color_white(),LV_PART_MAIN);
     lv_obj_set_style_text_opa(text,LV_OPA_COVER,LV_PART_MAIN);
     lv_label_set_text_fmt(text,"%d%s",lv_arc_get_value(Arc_In),"%");
-    lv_obj_align_to(text,Center,LV_ALIGN_CENTER,0,15);
+    lv_obj_set_style_text_font(text,&lv_font_montserrat_10,LV_PART_MAIN);
+    lv_obj_align_to(text,Center,LV_ALIGN_BOTTOM_MID,-3,15);
 
 
-
-    Btn = lv_btn_create(Arc_In);
+    Btn = lv_btn_create(ALL_Bg);
     Btn_Start = lv_label_create(Btn);
-
-    lv_obj_set_size(Btn,15,15);
+    lv_obj_set_size(Btn,8,10);
+    lv_obj_set_style_radius(Btn,0,LV_PART_MAIN);
     lv_label_set_text(Btn_Start,"V");
-    lv_obj_align(Btn,LV_ALIGN_BOTTOM_RIGHT,-10,0);
+    lv_obj_set_style_text_font(Btn_Start,&lv_font_montserrat_10,LV_PART_MAIN);
     lv_obj_align(Btn_Start,LV_ALIGN_CENTER,0,0);
     lv_obj_add_event_cb(Btn,Btn_CB,LV_EVENT_PRESSED,NULL);
     lv_obj_add_flag(Btn,LV_OBJ_FLAG_HIDDEN);
-    lv_obj_add_event_cb(Arc_Out,Arc_Out_CB,LV_EVENT_DRAW_MAIN_END,NULL);
+    lv_obj_align_to(Btn,Arc_In,LV_ALIGN_BOTTOM_RIGHT,-13,0);
+    
+
+    Atext = lv_label_create(ALL_Bg);
+    lv_obj_set_style_text_color(Atext,lv_color_white(),LV_PART_MAIN);
+    lv_obj_set_style_flex_flow(Center,LV_FLEX_FLOW_ROW,LV_PART_MAIN);
+    lv_label_set_text_fmt(Atext,LV_SYMBOL_DOWN"\n"LV_SYMBOL_DOWN"\n"LV_SYMBOL_DOWN);   
+    lv_obj_set_style_text_font(Atext,&lv_font_montserrat_10,LV_PART_MAIN);
+    lv_obj_align_to(Atext,Center,LV_ALIGN_CENTER,0,0);
 
 
     lv_anim_init(&Anima);
 
     Group = lv_group_create();
     lv_group_add_obj(Group,Arc_In);
-    lv_group_add_obj(Group,Btn);
-    lv_group_focus_obj(Arc_In);
     lv_indev_set_group(lv_win32_encoder_device_object,Group);
 
 }
