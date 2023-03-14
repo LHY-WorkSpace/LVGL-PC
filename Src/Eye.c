d4f963cecb91179bd67725504acfcdfa57b0aea8
LHY-WorkSpace <2371270263@qq.com>
2023-03-14 22:39:13 +0800

    2023.03.14 1.完善眼部旋转动画，次数和角度可控

--- Src/Eye.c
+++ Src/Eye.c
@@ -1,73 +1,251 @@
 #include "LVGL_UI.h"
 
-#define EYE_SIZE_W    (lv_obj_get_width(lv_scr_act())/4)
+
+#define RADTODEG(x) ((x) * 57.295779513082320876798154814105f)
+#define DEGTORAD(x) ((x) * 0.01745329251994329576923690768489f)
+
+#define FAST_SIN_TABLE_SIZE 512
+
+const float sinTable[FAST_SIN_TABLE_SIZE + 1] = 
+{
+	0.00000000f, 0.01227154f, 0.02454123f, 0.03680722f, 0.04906767f, 0.06132074f,
+	0.07356456f, 0.08579731f, 0.09801714f, 0.11022221f, 0.12241068f, 0.13458071f,
+	0.14673047f, 0.15885814f, 0.17096189f, 0.18303989f, 0.19509032f, 0.20711138f,
+	0.21910124f, 0.23105811f, 0.24298018f, 0.25486566f, 0.26671276f, 0.27851969f,
+	0.29028468f, 0.30200595f, 0.31368174f, 0.32531029f, 0.33688985f, 0.34841868f,
+	0.35989504f, 0.37131719f, 0.38268343f, 0.39399204f, 0.40524131f, 0.41642956f,
+	0.42755509f, 0.43861624f, 0.44961133f, 0.46053871f, 0.47139674f, 0.48218377f,
+	0.49289819f, 0.50353838f, 0.51410274f, 0.52458968f, 0.53499762f, 0.54532499f,
+	0.55557023f, 0.56573181f, 0.57580819f, 0.58579786f, 0.59569930f, 0.60551104f,
+	0.61523159f, 0.62485949f, 0.63439328f, 0.64383154f, 0.65317284f, 0.66241578f,
+	0.67155895f, 0.68060100f, 0.68954054f, 0.69837625f, 0.70710678f, 0.71573083f,
+	0.72424708f, 0.73265427f, 0.74095113f, 0.74913639f, 0.75720885f, 0.76516727f,
+	0.77301045f, 0.78073723f, 0.78834643f, 0.79583690f, 0.80320753f, 0.81045720f,
+	0.81758481f, 0.82458930f, 0.83146961f, 0.83822471f, 0.84485357f, 0.85135519f,
+	0.85772861f, 0.86397286f, 0.87008699f, 0.87607009f, 0.88192126f, 0.88763962f,
+	0.89322430f, 0.89867447f, 0.90398929f, 0.90916798f, 0.91420976f, 0.91911385f,
+	0.92387953f, 0.92850608f, 0.93299280f, 0.93733901f, 0.94154407f, 0.94560733f,
+	0.94952818f, 0.95330604f, 0.95694034f, 0.96043052f, 0.96377607f, 0.96697647f,
+	0.97003125f, 0.97293995f, 0.97570213f, 0.97831737f, 0.98078528f, 0.98310549f,
+	0.98527764f, 0.98730142f, 0.98917651f, 0.99090264f, 0.99247953f, 0.99390697f,
+	0.99518473f, 0.99631261f, 0.99729046f, 0.99811811f, 0.99879546f, 0.99932238f,
+	0.99969882f, 0.99992470f, 1.00000000f, 0.99992470f, 0.99969882f, 0.99932238f,
+	0.99879546f, 0.99811811f, 0.99729046f, 0.99631261f, 0.99518473f, 0.99390697f,
+	0.99247953f, 0.99090264f, 0.98917651f, 0.98730142f, 0.98527764f, 0.98310549f,
+	0.98078528f, 0.97831737f, 0.97570213f, 0.97293995f, 0.97003125f, 0.96697647f,
+	0.96377607f, 0.96043052f, 0.95694034f, 0.95330604f, 0.94952818f, 0.94560733f,
+	0.94154407f, 0.93733901f, 0.93299280f, 0.92850608f, 0.92387953f, 0.91911385f,
+	0.91420976f, 0.90916798f, 0.90398929f, 0.89867447f, 0.89322430f, 0.88763962f,
+	0.88192126f, 0.87607009f, 0.87008699f, 0.86397286f, 0.85772861f, 0.85135519f,
+	0.84485357f, 0.83822471f, 0.83146961f, 0.82458930f, 0.81758481f, 0.81045720f,
+	0.80320753f, 0.79583690f, 0.78834643f, 0.78073723f, 0.77301045f, 0.76516727f,
+	0.75720885f, 0.74913639f, 0.74095113f, 0.73265427f, 0.72424708f, 0.71573083f,
+	0.70710678f, 0.69837625f, 0.68954054f, 0.68060100f, 0.67155895f, 0.66241578f,
+	0.65317284f, 0.64383154f, 0.63439328f, 0.62485949f, 0.61523159f, 0.60551104f,
+	0.59569930f, 0.58579786f, 0.57580819f, 0.56573181f, 0.55557023f, 0.54532499f,
+	0.53499762f, 0.52458968f, 0.51410274f, 0.50353838f, 0.49289819f, 0.48218377f,
+	0.47139674f, 0.46053871f, 0.44961133f, 0.43861624f, 0.42755509f, 0.41642956f,
+	0.40524131f, 0.39399204f, 0.38268343f, 0.37131719f, 0.35989504f, 0.34841868f,
+	0.33688985f, 0.32531029f, 0.31368174f, 0.30200595f, 0.29028468f, 0.27851969f,
+	0.26671276f, 0.25486566f, 0.24298018f, 0.23105811f, 0.21910124f, 0.20711138f,
+	0.19509032f, 0.18303989f, 0.17096189f, 0.15885814f, 0.14673047f, 0.13458071f,
+	0.12241068f, 0.11022221f, 0.09801714f, 0.08579731f, 0.07356456f, 0.06132074f,
+	0.04906767f, 0.03680722f, 0.02454123f, 0.01227154f, 0.00000000f, -0.01227154f,
+	-0.02454123f, -0.03680722f, -0.04906767f, -0.06132074f, -0.07356456f,
+	-0.08579731f, -0.09801714f, -0.11022221f, -0.12241068f, -0.13458071f,
+	-0.14673047f, -0.15885814f, -0.17096189f, -0.18303989f, -0.19509032f, 
+	-0.20711138f, -0.21910124f, -0.23105811f, -0.24298018f, -0.25486566f, 
+	-0.26671276f, -0.27851969f, -0.29028468f, -0.30200595f, -0.31368174f, 
+	-0.32531029f, -0.33688985f, -0.34841868f, -0.35989504f, -0.37131719f, 
+	-0.38268343f, -0.39399204f, -0.40524131f, -0.41642956f, -0.42755509f, 
+	-0.43861624f, -0.44961133f, -0.46053871f, -0.47139674f, -0.48218377f, 
+	-0.49289819f, -0.50353838f, -0.51410274f, -0.52458968f, -0.53499762f, 
+	-0.54532499f, -0.55557023f, -0.56573181f, -0.57580819f, -0.58579786f, 
+	-0.59569930f, -0.60551104f, -0.61523159f, -0.62485949f, -0.63439328f, 
+	-0.64383154f, -0.65317284f, -0.66241578f, -0.67155895f, -0.68060100f, 
+	-0.68954054f, -0.69837625f, -0.70710678f, -0.71573083f, -0.72424708f, 
+	-0.73265427f, -0.74095113f, -0.74913639f, -0.75720885f, -0.76516727f, 
+	-0.77301045f, -0.78073723f, -0.78834643f, -0.79583690f, -0.80320753f, 
+	-0.81045720f, -0.81758481f, -0.82458930f, -0.83146961f, -0.83822471f, 
+	-0.84485357f, -0.85135519f, -0.85772861f, -0.86397286f, -0.87008699f, 
+	-0.87607009f, -0.88192126f, -0.88763962f, -0.89322430f, -0.89867447f, 
+	-0.90398929f, -0.90916798f, -0.91420976f, -0.91911385f, -0.92387953f, 
+	-0.92850608f, -0.93299280f, -0.93733901f, -0.94154407f, -0.94560733f, 
+	-0.94952818f, -0.95330604f, -0.95694034f, -0.96043052f, -0.96377607f, 
+	-0.96697647f, -0.97003125f, -0.97293995f, -0.97570213f, -0.97831737f, 
+	-0.98078528f, -0.98310549f, -0.98527764f, -0.98730142f, -0.98917651f, 
+	-0.99090264f, -0.99247953f, -0.99390697f, -0.99518473f, -0.99631261f, 
+	-0.99729046f, -0.99811811f, -0.99879546f, -0.99932238f, -0.99969882f, 
+	-0.99992470f, -1.00000000f, -0.99992470f, -0.99969882f, -0.99932238f, 
+	-0.99879546f, -0.99811811f, -0.99729046f, -0.99631261f, -0.99518473f, 
+	-0.99390697f, -0.99247953f, -0.99090264f, -0.98917651f, -0.98730142f, 
+	-0.98527764f, -0.98310549f, -0.98078528f, -0.97831737f, -0.97570213f, 
+	-0.97293995f, -0.97003125f, -0.96697647f, -0.96377607f, -0.96043052f, 
+	-0.95694034f, -0.95330604f, -0.94952818f, -0.94560733f, -0.94154407f, 
+	-0.93733901f, -0.93299280f, -0.92850608f, -0.92387953f, -0.91911385f, 
+	-0.91420976f, -0.90916798f, -0.90398929f, -0.89867447f, -0.89322430f, 
+	-0.88763962f, -0.88192126f, -0.87607009f, -0.87008699f, -0.86397286f, 
+	-0.85772861f, -0.85135519f, -0.84485357f, -0.83822471f, -0.83146961f, 
+	-0.82458930f, -0.81758481f, -0.81045720f, -0.80320753f, -0.79583690f, 
+	-0.78834643f, -0.78073723f, -0.77301045f, -0.76516727f, -0.75720885f, 
+	-0.74913639f, -0.74095113f, -0.73265427f, -0.72424708f, -0.71573083f, 
+	-0.70710678f, -0.69837625f, -0.68954054f, -0.68060100f, -0.67155895f, 
+	-0.66241578f, -0.65317284f, -0.64383154f, -0.63439328f, -0.62485949f, 
+	-0.61523159f, -0.60551104f, -0.59569930f, -0.58579786f, -0.57580819f, 
+	-0.56573181f, -0.55557023f, -0.54532499f, -0.53499762f, -0.52458968f, 
+	-0.51410274f, -0.50353838f, -0.49289819f, -0.48218377f, -0.47139674f, 
+	-0.46053871f, -0.44961133f, -0.43861624f, -0.42755509f, -0.41642956f, 
+	-0.40524131f, -0.39399204f, -0.38268343f, -0.37131719f, -0.35989504f, 
+	-0.34841868f, -0.33688985f, -0.32531029f, -0.31368174f, -0.30200595f, 
+	-0.29028468f, -0.27851969f, -0.26671276f, -0.25486566f, -0.24298018f, 
+	-0.23105811f, -0.21910124f, -0.20711138f, -0.19509032f, -0.18303989f, 
+	-0.17096189f, -0.15885814f, -0.14673047f, -0.13458071f, -0.12241068f, 
+	-0.11022221f, -0.09801714f, -0.08579731f, -0.07356456f, -0.06132074f, 
+	-0.04906767f, -0.03680722f, -0.02454123f, -0.01227154f, -0.00000000f
+};
 
 
+float FastSin(float x)
+{
+	float sinVal, fract, in; // Temporary variables for input, output
+	unsigned short index; // Index variable
+	float a, b; // Two nearest output values
+	int n;
+	float findex;
 
-lv_obj_t *Eye_G[4];
+	// input x is in radians
+	// Scale the input to [0 1] range from [0 2*PI] , divide input by 2*pi
+	in = x * 0.159154943092f;
 
+	// Calculation of floor value of input
+	n = (int) in;
+
+	// Make negative values towards -infinity
+	if(x < 0.0f){
+		n--;
+	}
+
+	// Map input value to [0 1]
+	in = in - (float) n;
+
+	// Calculation of index of the table
+	findex = (float) FAST_SIN_TABLE_SIZE * in;
+	index = ((unsigned short)findex) & 0x1ff;
+
+	// fractional value calculation
+	fract = findex - (float) index;
+
+	// Read two nearest values of input value from the sin table
+	a = sinTable[index];
+	b = sinTable[index+1];
+
+	// Linear interpolation process
+	sinVal = (1.0f-fract)*a + fract*b;
+
+	// Return the output value
+	return (sinVal);
+}
+
+float FastCos(float x)
+{
+	float cosVal, fract, in; // Temporary variables for input, output
+	unsigned short index; // Index variable
+	float a, b; // Two nearest output values
+	int n;
+	float findex;
+
+	// input x is in radians
+	// Scale the input to [0 1] range from [0 2*PI] , divide input by 2*pi, add 0.25 (pi/2) to read sine table
+	in = x * 0.159154943092f + 0.25f;
+
+	// Calculation of floor value of input
+	n = (int) in;
+
+	// Make negative values towards -infinity
+	if(in < 0.0f){
+		n--;
+	}
+
+	// Map input value to [0 1]
+	in = in - (float) n;
+
+	// Calculation of index of the table
+	findex = (float) FAST_SIN_TABLE_SIZE * in;
+	index = ((unsigned short)findex) & 0x1ff;
+
+	// fractional value calculation
+	fract = findex - (float) index;
+
+	// Read two nearest values of input value from the cos table
+	a = sinTable[index];
+	b = sinTable[index+1];
+
+	// Linear interpolation process
+	cosVal = (1.0f-fract)*a + fract*b;
+
+	// Return the output value
+	return (cosVal);
+}
+
+
+// 逆时针（如下图）：
+// x1=xcos(β)-ysin(β);
+// y1=ycos(β)+xsin(β);
+
+// 顺时针（图未给出）：
+// x1=xcos(β)+ysin(β);
+// y1=ycos(β)-xsin(β);
+
+
+//旋转半径
+#define R_LEN (70)
+#define EYE_SIZE_W    (60)
 
 lv_obj_t *Eye_Group[4];
 lv_anim_t Anima_Eye_Group[4];
+uint8_t RotateDir = 1;
 
-int8_t Eye_Position[4][2]=
+
+int8_t Eye_Position[4][3]=
 {
-    {0,-70},    //上
-    {70,0},     //右
-    {0,70},     //下
-    {-70,0},     //左
+    {0,-R_LEN,0},     //上
+    {R_LEN,0, 1},     //右
+    {0,R_LEN, 2},     //下
+    {-R_LEN,0,3},     //左
 };
 
 
 static void Animation_CB(void *var, int32_t v)
 {
     uint8_t i,j;
-    int16_t x,y;
-   static uint8_t Flag = 0xff;
+    int16_t x,y,x1,y1;
 
     lv_obj_t *Eye_tmp = (lv_obj_t *)var;
 
-
-    x = lv_obj_get_x(Eye_Group[0]);
-    y = lv_obj_get_y(Eye_Group[0]);
-
     for ( i = 0; i < 4; i++)
     {
-        if( Eye_tmp == Eye_Group[0] )//eye[0]
+        if(Eye_tmp == Eye_Group[i])
         {
-            for ( j = 0; j < 4; j++)
+            if( RotateDir == 0)
             {
-                if( x == Eye_Position[j][0] && y == Eye_Position[j][1])//找eye[0]的基准位置，
-                {
-                    Flag = j;
-                }
+                x1=(Eye_Position[i][0])*FastCos(DEGTORAD(v))-(Eye_Position[i][1])*FastSin(DEGTORAD(v));
+                y1=(Eye_Position[i][1])*FastCos(DEGTORAD(v))+(Eye_Position[i][0])*FastSin(DEGTORAD(v));  
+            }
+            else
+            {
+                x1=(Eye_Position[i][0])*FastCos(DEGTORAD(v))+(Eye_Position[i][1])*FastSin(DEGTORAD(v));
+                y1=(Eye_Position[i][1])*FastCos(DEGTORAD(v))-(Eye_Position[i][0])*FastSin(DEGTORAD(v));  
             }
 
-            if(Flag != 0xff)//到达基准点位置
-            {
-                for( i = 0; i < 4; i++)
-                {
-                    //重新设置每个eye的位置
-                    lv_obj_align_to(Eye_Group[i],Face,LV_ALIGN_CENTER, Eye_Position[(Flag+i)%4][0], Eye_Position[(Flag+i)%4][1]);
-                }
 
-                break;
-            }
-            else//未到达基准点位置,执行移动效果
+            lv_obj_align_to(Eye_Group[Eye_Position[i][2]],Face,LV_ALIGN_CENTER,x1,y1);
+
+            if( (x1 == Eye_Position[(i+1)%4][0] ) && (y1 == Eye_Position[(i+1)%4][1]))
             {
-                for( i = 0; i < 4; i++)
-                {
-                    lv_obj_align_to(Eye_Group[i],Face,LV_ALIGN_CENTER, Eye_Position[(Flag+i+1)%4][0]+v, Eye_Position[(Flag+i+1)%4][1]+v);
-                }
+                Eye_Position[i][2] = (i+1)%4;
             }
         }
     }
-
-    // lv_obj_align_to(Eye_Group[0],Face,LV_ALIGN_CENTER,0-v,v-70);
-    // lv_obj_align_to(Eye_Group[1],Face,LV_ALIGN_CENTER,70-v,0-v);
-    // lv_obj_align_to(Eye_Group[2],Face,LV_ALIGN_CENTER,0+v,70-v);
-    // lv_obj_align_to(Eye_Group[3],Face,LV_ALIGN_CENTER,-70+v,0+v);
-
 }
 
 
@@ -75,6 +253,7 @@ static void Animation_CB(void *var, int32_t v)
 void Eye_Create()
 {
     uint8_t i=0;
+    int16_t x,y,x1,y1;
 
     for ( i = 0; i < 4; i++)
     {
@@ -89,10 +268,10 @@ void Eye_Create()
 
         lv_anim_init(&Anima_Eye_Group[i]);
         lv_anim_set_var(&Anima_Eye_Group[i],Eye_Group[i]);
-        lv_anim_set_values(&Anima_Eye_Group[i],0,70);
+        lv_anim_set_values(&Anima_Eye_Group[i],0,90);
         lv_anim_set_time(&Anima_Eye_Group[i], 500);
         lv_anim_set_exec_cb(&Anima_Eye_Group[i], Animation_CB);
-        lv_anim_set_path_cb(&Anima_Eye_Group[i],lv_anim_path_ease_in_out);
+        lv_anim_set_path_cb(&Anima_Eye_Group[i],lv_anim_path_bounce);
 
         lv_anim_set_repeat_delay(&Anima_Eye_Group[i],500);
         lv_anim_set_repeat_count(&Anima_Eye_Group[i], LV_ANIM_PLAYTIME_INFINITE);
@@ -105,12 +284,7 @@ void Eye_Create()
     }
 
 
-    // lv_obj_align_to(Eye_Group[i],Face,LV_ALIGN_CENTER,70,0);
-    // lv_obj_align_to(Eye_Group[i],Face,LV_ALIGN_CENTER,0,70);
-    // lv_obj_align_to(Eye_Group[i],Face,LV_ALIGN_CENTER,-70,0);
-
-
-    // lv_anim_set_path_cb(&Anima_Eye1,lv_anim_path_ease_in_out);
+    // lv_anim_set_path_cb(&Anima,lv_anim_path_ease_in_out);
     // lv_anim_set_path_cb(&Anima,lv_anim_path_ease_in);
     // lv_anim_set_path_cb(&Anima,lv_anim_path_ease_out);//==
     // lv_anim_set_path_cb(&Anima,lv_anim_path_bounce);//==
